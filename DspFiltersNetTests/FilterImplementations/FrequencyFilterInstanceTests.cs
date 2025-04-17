ff449af3bea7075742e5c8249a7d1c47db1d40b4
Uight <uightuight@gmail.com>
2025-04-17 23:11:53 +0200

    Add and adjust tests from old project

---
+++ DspFiltersNetTests/FilterImplementations/FrequencyFilterInstanceTests.cs
@@ -0,0 +1,122 @@
+﻿using DspFiltersNet.Filter;
+using DspFiltersNet.FilterImplementations;
+using System.ComponentModel;
+
+namespace DspFiltersNetTests.FilterImplementations;
+
+//This test only tests the basic function of the filter instance. Not all filter configurations are tested as the configuration itself is tested by the FilterTools Test
+//Therefore it is enough to just test the instance with on filter configuration as the other will just work the same
+
+[TestFixture]
+internal class FrequencyFilterInstanceTests
+{
+    private static readonly double[] ExpectedValuesNormal = [0.0000000000000000D, 0.0005612709253673D, 0.0031056862801681D, 0.0087206296400223D, 0.0171477912799752D];
+    private static readonly double[] ExpectedValuesWithNaN = [0.0000000000000000D, 0.0005612709253673D, 0.0031056862801681D, 0.0087206296400223D, 0.0171477912799752D, double.NaN, 0.0000000000000000D, 0.0005612709253673D, 0.0031056862801681D, 0.0087206296400223D, 0.0171477912799752D];
+    
+    [Test]
+    public void FrequencyFilterInstance_Process_CalculatesCorrectFiltered()
+    {
+        var filterData = new FrequencyFilterDefinition(FrequencyFilterType.LowPass, 10, 100, FrequencyFilterDesignType.Butterworth, 2, 1000);
+        var filterInstance = new FrequencyFilterInstance(filterData);
+        
+        const double tolerance = 1E-12d;
+        
+        var result1 = filterInstance.Process(0.0000000000000000D);
+        var result2 = filterInstance.Process(0.5941312281110626D);
+        var result3 = filterInstance.Process(0.9637482198843191D);
+        var result4 = filterInstance.Process(0.9700934515686609D);
+        var result5 = filterInstance.Process(0.6131666751454520D);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(result1, Is.EqualTo(0.0000000000000000D).Within(tolerance));
+            Assert.That(result2, Is.EqualTo(0.0005612709253673D).Within(tolerance));
+            Assert.That(result3, Is.EqualTo(0.0031056862801681D).Within(tolerance));
+            Assert.That(result4, Is.EqualTo(0.0087206296400223D).Within(tolerance));
+            Assert.That(result5, Is.EqualTo(0.0171477912799752D).Within(tolerance));
+        });
+    }
+    
+    [Test]
+    public void FrequencyFilterInstance_ProcessCurve_CalculatesCorrectFiltered()
+    {
+        var filterData = new FrequencyFilterDefinition(FrequencyFilterType.LowPass, 10, 100, FrequencyFilterDesignType.Butterworth, 2, 1000);
+        var filterInstance = new FrequencyFilterInstance(filterData);
+        
+        const double tolerance = 1E-12d;
+
+        var result = filterInstance.ProcessSamples([0.0000000000000000D, 0.5941312281110626D, 0.9637482198843191D, 0.9700934515686609D, 0.6131666751454520D]);
+        Assert.That(result, Is.EqualTo(ExpectedValuesNormal).Within(tolerance));
+    }
+    
+        
+    [Test]
+    public void FrequencyFilterInstance_ProcessCurveWithNaN_CalculatesCorrectFiltered()
+    {
+        var filterData = new FrequencyFilterDefinition(FrequencyFilterType.LowPass, 10, 100, FrequencyFilterDesignType.Butterworth, 2, 1000);
+        var filterInstance = new FrequencyFilterInstance(filterData);
+        
+        const double tolerance = 1E-12d;
+
+        var result = filterInstance.ProcessSamples([0.0000000000000000D, 0.5941312281110626D, 0.9637482198843191D, 0.9700934515686609D, 0.6131666751454520D, double.NaN, 0.0000000000000000D, 0.5941312281110626D, 0.9637482198843191D, 0.9700934515686609D, 0.6131666751454520D]);
+        Assert.That(result, Is.EqualTo(ExpectedValuesWithNaN).Within(tolerance));
+    }
+
+    [Test]
+    public void FrequencyFilterInstance_ResetFilter()
+    {
+        var filterData = new FrequencyFilterDefinition(FrequencyFilterType.LowPass, 10, 100, FrequencyFilterDesignType.Butterworth, 2, 1000);
+        var filterInstance = new FrequencyFilterInstance(filterData);
+
+        const double tolerance = 1E-12d;
+
+        filterInstance.Process(0.0000000000000000D);
+        filterInstance.Process(0.5941312281110626D);
+        filterInstance.Process(0.9637482198843191D);
+
+        filterInstance.ResetCalculation();
+        filterInstance.Process(0.0000000000000000D);
+        var result = filterInstance.Process(0.5941312281110626D);
+        
+        Assert.That(result, Is.EqualTo(0.0005612709253673D).Within(tolerance));
+    }
+    
+    [Test]
+    public void FrequencyFilterInstance_ResetFilter_WithValueAfterReset()
+    {
+        var filterData = new FrequencyFilterDefinition(FrequencyFilterType.LowPass, 10, 100, FrequencyFilterDesignType.Butterworth, 2, 1000);
+        var filterInstance = new FrequencyFilterInstance(filterData);
+
+        const double tolerance = 1E-12d;
+
+        filterInstance.Process(0.0000000000000000D);
+        filterInstance.Process(0.5941312281110626D);
+        filterInstance.Process(0.9637482198843191D);
+
+        filterInstance.ResetCalculation();
+        var result1 = filterInstance.Process(1.0000000000000000D);
+        var result2 = filterInstance.Process(1.5941312281110626D);
+        var result3 = filterInstance.Process(1.9637482198843191D);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(result1, Is.EqualTo(1.0D).Within(tolerance));
+            Assert.That(result2, Is.EqualTo(1.0005612709253673D).Within(tolerance));
+            Assert.That(result3, Is.EqualTo(1.0031056862801681D).Within(tolerance));
+        });
+    }
+
+    [Test]
+    public void FrequencyFilterInstance_InvalidDesignType_ThrowsException()
+    {
+        var invalidFilterData = new FrequencyFilterDefinition(FrequencyFilterType.LowPass, 10, 100, (FrequencyFilterDesignType)5, 2, 1000);
+        Assert.Throws<InvalidEnumArgumentException>(() => { _ = new FrequencyFilterInstance(invalidFilterData); });
+    }
+    
+    [Test]
+    public void FrequencyFilterInstance_InvalidCutOffFrequenciesSetting_ThrowsException()
+    {
+        var invalidFilterData = new FrequencyFilterDefinition(FrequencyFilterType.BandPass, 100, 100, FrequencyFilterDesignType.Butterworth, 2, 1000);
+        Assert.Throws<ArgumentOutOfRangeException>(() => { _ = new FrequencyFilterInstance(invalidFilterData); });
+    }
+}
---
+++ DspFiltersNetTests/FilterImplementations/MovingAverageInstanceTests.cs
@@ -0,0 +1,75 @@
+﻿using DspFiltersNet.Filter;
+using DspFiltersNet.FilterImplementations;
+
+namespace DspFiltersNetTests.FilterImplementations;
+
+[TestFixture]
+internal class MovingAverageInstanceTests
+{
+    private static readonly double[] ExpectedValuesNormal = [1.0, 1.5, 2.0, 3.0, 4.0];
+    private static readonly double[] ExpectedValuesWithNaN = [1.0, 1.5, 2.0, 3.0, 4.0, double.NaN, 1.0, 1.5, 2.0, 3.0, 4.0];
+    
+    [Test]
+    public void MovingAverageFilter_Process_CalculatesCorrectAverage()
+    {
+        var filterData = new MovingAverageFilterDefinition(3);
+        var filterInstance = new MovingAverageInstance(filterData);
+        
+        var result1 = filterInstance.Process(1.0);
+        var result2 = filterInstance.Process(2.0);
+        var result3 = filterInstance.Process(3.0);
+        var result4 = filterInstance.Process(4.0);
+        var result5 = filterInstance.Process(5.0);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(result1, Is.EqualTo(1.0));
+            Assert.That(result2, Is.EqualTo(1.5));
+            Assert.That(result3, Is.EqualTo(2.0));
+            Assert.That(result4, Is.EqualTo(3.0));
+            Assert.That(result5, Is.EqualTo(4.0));
+        });
+    }
+    
+    [Test]
+    public void MovingAverageFilter_ProcessCurve_CalculatesCorrectAverage()
+    {
+        var filterData = new MovingAverageFilterDefinition(3);
+        var filterInstance = new MovingAverageInstance(filterData);
+
+        var result = filterInstance.ProcessSamples([1.0, 2.0, 3.0, 4.0, 5.0]);
+        Assert.That(result, Is.EqualTo(ExpectedValuesNormal));
+    }
+    
+    [Test]
+    public void MovingAverageFilter_ProcessCurveWithNaN_CalculatesCorrectAverage()
+    {
+        var filterData = new MovingAverageFilterDefinition(3);
+        var filterInstance = new MovingAverageInstance(filterData);
+
+        var result = filterInstance.ProcessSamples([1.0, 2.0, 3.0, 4.0, 5.0, double.NaN, 1.0, 2.0, 3.0, 4.0, 5.0]);
+        Assert.That(result, Is.EqualTo(ExpectedValuesWithNaN));
+    }
+    
+    [Test]
+    public void MovingAverageFilter_ResetFilter()
+    {
+        var filterData = new MovingAverageFilterDefinition(3);
+        var filterInstance = new MovingAverageInstance(filterData);
+        
+        filterInstance.Process(1.0);
+        filterInstance.Process(2.0);
+        filterInstance.Process(3.0);
+        
+        filterInstance.ResetCalculation();
+        
+        Assert.That(filterInstance.Process(1.0), Is.EqualTo(1.0));
+    }
+
+    [TestCase(0)]
+    public void MovingAverageFilter_InvalidWidth_ThrowsException(int width)
+    {
+        var invalidFilterData = new MovingAverageFilterDefinition(width);
+        Assert.Throws<ArgumentOutOfRangeException>(() => { _ = new MovingAverageInstance(invalidFilterData); });
+    }
+}
